d8beaae6548fa6d463215e649273458bacda11d2
Panny8 <111778724+Panny8@users.noreply.github.com>
2023-06-29 22:42:00 +0100

    made changes

--- components/Header.js
+++ components/Header.js
@@ -70,4 +70,4 @@ export default function Header() {
       </div>
     </header>
   )
-}
+    }
--- firebase.js
+++ firebase.js
@@ -1,10 +1,21 @@
-import firebase from 'firebase'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAC8hAdt5rTKF7tdJ2N01E5_JiXN9YVTtg",
-    authDomain: "fir-b1bdb.firebaseapp.com",
-    projectId: "fir-b1bdb",
-    storageBucket: "fir-b1bdb.appspot.com",
-    messagingSenderId: "588941789635",
-    appId: "1:588941789635:web:1baa0dcd8ec2a160121576",
-    measurementId: "G-FL77R3YJX5"
-  };
+  apiKey: "AIzaSyBRSxCepZsbzICZOtX0X7-V67o7p2Rw44E",
+  authDomain: "panny-33070.firebaseapp.com",
+  projectId: "panny-33070",
+  storageBucket: "panny-33070.appspot.com",
+  messagingSenderId: "895915555579",
+  appId: "1:895915555579:web:33a8297c69256da43c0e5e",
+  measurementId: "G-GMRVM3Z2G9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
